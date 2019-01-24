22207a98f4371e7752952e3902976cfbe085f468
Sean Chiem <seanchiem@seans-mbp.attlocal.net>
2019-01-24 15:04:16 -0500

    added robofriends project

---
+++ build/precache-manifest.2871b8e1d33abcace35e7ef4514f37b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a284c7e64636eafa1873",
+    "url": "/static/js/main.a284c7e6.chunk.js"
+  },
+  {
+    "revision": "bbd50ac9f5a82dd2938b",
+    "url": "/static/js/1.bbd50ac9.chunk.js"
+  },
+  {
+    "revision": "a284c7e64636eafa1873",
+    "url": "/static/css/main.e44e642e.chunk.css"
+  },
+  {
+    "revision": "bbd50ac9f5a82dd2938b",
+    "url": "/static/css/1.a27566f3.chunk.css"
+  },
+  {
+    "revision": "0de8d888720a1523c9e0213805e5e69b",
+    "url": "/index.html"
+  }
+];
